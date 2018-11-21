558fb9b98de05b601cd6c7cb22f51a4e2f9532fe
Song-Hyunsung <song.hyunsung96@gmail.com>
2018-11-20 22:27:44 -0500

    add /:id get put delete route for task

--- controllers/task.js
+++ controllers/task.js
@@ -28,4 +28,42 @@ router.post('/', (req, res) => {
 	});
 });
 
+router.get('/:id', (req, res) => {
+	Task.findById(req.params.id).then(task => {
+		res.json({
+			id: task.id,
+			title: task.task_title,
+			description: task.task_description,	
+		});
+	}).catch(() => {
+		res.status(400).json({ msg: "task with id does not exist: " + req.params.id })
+	});
+});
+
+router.put('/:id', (req, res) => {
+	Task.findById(req.params.id).then(task => {
+		task.update({
+			task_title: req.body.task_title,
+			task_description: req.body.task_description,
+		}).then(updated_task => {
+			res.json({
+				updated_task
+			});
+		});
+	}).catch(() => {
+		res.status(400).json({ msg: "task with id does not exist: " + req.params.id })
+	});
+});
+
+router.delete('/:id', (req, res) => {
+	Task.findById(req.params.id).then(task => {
+		task.destroy();
+		res.json({
+			msg: "task with id " + req.params.id + " deleted from database"
+		});
+	}).catch(() => {
+		res.status(400).json({ msg: "task with id does not exist: " + req.params.id })
+	});
+});
+
 module.exports = router;
