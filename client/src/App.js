579c496c241d2b54d86778defdb6972e9501602f
Song-Hyunsung <song.hyunsung96@gmail.com>
2018-11-28 22:03:09 -0500

    simple react practice

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  state = {
-    data: null
-  };
+class MinorApp extends React.Component {
+  render(){
+    const randomList = ["A", "B", "C", "D", "E"][Math.floor(Math.random() * 5)];
+    const whatToDo = ["breathe", "stop", "idk", "open window"];
+    const inLineTest = {fontSize: '2.5em', margin: '20px', backgroundColor: 'green', color: 'red'}
 
-  componentDidMount(){
-    this.callBackendAPI()
-      .then(res => this.setState({ data : res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if(response.status !== 200){
-      throw Error(body.message)
-    }
-
-    return body;
-  };
-
-
-
-
-
-  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p className="App-intro">{this.state.data}</p>
+      <ul>
+        { randomList }
+        { whatToDo.map((element, i) => {
+          return <li key={i} style={inLineTest}>{element}</li>
+        })}
+      </ul>
+    );
+  }
+}
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <h2>Simple Test</h2>
+        <MinorApp />
       </div>
     );
   }
