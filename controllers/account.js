5f7b7aa4cd1784c6db5fd26f539e95b69e2e5452
Song-Hyunsung <song.hyunsung96@gmail.com>
2018-11-29 02:06:17 -0500

    refactor backend api routes

---
+++ controllers/account.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const models = require('../models');
+const passport = require('../middlewares/auth');
+
+const router = express.Router();
+const User = models.User;
+
+router.get('/signup', (req, res) => {
+	res.json({
+		msg : "Succesful route to /api/signup GET"
+	});
+});
+
+router.post('/signup', (req, res) => {
+	User.create({
+		username: req.body.username,
+		password_hash: req.body.password,
+	}).then((user) => {
+		res.json({ msg: "User signed" + user.id + user.username });
+	}).catch(() => {
+		res.status(400);
+	});
+});
+
+router.post('/login',
+	passport.authenticate('local', { failureRedirect: '/api/signup' }),
+	(req, res) => {
+		res.json({
+			msg: "Succesfully authenticated",
+			id: req.user.id,
+			username: req.user.username,
+		});
+	});
+
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.sendStatus(200);
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('./home'));
-router.use('/task', require('./task'));
-router.use('/auth', require('./auth'));
+router.use('/api', require('./account'));
+router.use('/api/profile', require('./profile'));
 
 module.exports = router;
